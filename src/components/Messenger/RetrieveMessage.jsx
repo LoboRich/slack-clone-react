1434ff122f081d9aa08be5b1a26ccb9c55d3fe1c
Richelle Lobo <lobo.r0018@gmail.com>
2021-09-30 21:03:37 +0800

    Retrieve Message with static data

--- src/components/Messenger/RetrieveMessage.jsx
+++ src/components/Messenger/RetrieveMessage.jsx
@@ -3,20 +3,14 @@ import { getToken } from "../../Utils/common";
 
 const RetrieveMessage = () => {
 
-    const data = {
-        'sender_id' : 809,
-        'receievr_class': 'User',
-        'receiver_id' : 809
-    }
-
     const recieveData = (e) => {
         e.preventDefault()
-        axios.get('http://206.189.91.54//api/v1/messages?receiver_class=User&receiver_id=1', data, {
-        headers: getToken()
-    }).then((res) => {
-        console.log(res['data'])
-    })
-}
+        axios.get('http://206.189.91.54//api/v1/messages?receiver_class=User&receiver_id=809',{
+            headers: getToken()
+        }).then((res) => {
+            console.log(res['data']['data'])
+        })
+    }
 
     return ( 
         <div className="retrieve-wrapper">
