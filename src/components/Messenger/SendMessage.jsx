f633fef73c77cf4b432288711bb74bb7d5bdd33e
Richelle Lobo <lobo.r0018@gmail.com>
2021-09-30 17:20:20 +0800

    Send message via post request

--- src/components/Messenger/SendMessage.jsx
+++ src/components/Messenger/SendMessage.jsx
@@ -3,19 +3,20 @@ import { getToken } from "../../Utils/common";
 
 const SendMessage = () => {
 
+    const data = {
+        'receiver_id': 54,
+        'receiver_class': 'User',
+        'body': 'Whats up!' 
+    }
     const SendMessage = (e) => {
         e.preventDefault()
-        axios.post('http://206.189.91.54//api/v1/messages', {
-            headers: getToken(),
-            body: {
-                'receiver_id': 54,
-                'receiver_class': 'User',
-                'body': 'Whats up!' 
-            }
-            }).then((res) => {
-                console.log(res['data'])
-            });
-        }
+        axios.post('http://206.189.91.54//api/v1/messages', data, {
+            headers: getToken()
+        })
+        .then((res) => {
+            console.log(res['data'])
+        });
+    }
 
     return ( 
         <div className="retrieve-wrapper">
