d876ae65ef27b68a60c9002059b6b638c3793311
Patrick Ranchez <ranchezpatrick@gmail.com>
2021-10-04 20:50:04 +0800

    changed ui for welcome component

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,7 +1,7 @@
 import './Welcome.css'
 import { Button } from '@material-ui/core'
 import { useHistory } from 'react-router-dom'
-
+import fina from '../resources/fina.gif'
 function Welcome() {
   const history = useHistory()
 
@@ -12,8 +12,8 @@ function Welcome() {
   return (
     <div className="Welcome">
       <div className="Welcome__container">
-        <img src="../resources/slack-icon-svg-24.svg" alt="Slack Logo" />
-        <h1>Welcome to Slack</h1>
+      <img className='slack-gif' src={fina} alt="" srcset="" />
+        <h1>Welcome to Slack!</h1>
         <p>
           Slack brings all your team communication into one place, makes it all
           instantly searchable and available wherever you go.
