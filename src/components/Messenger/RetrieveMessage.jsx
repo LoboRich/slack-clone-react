4777f1f35f4622ed4ded588b402a54d2a5f1870c
Richelle Lobo <lobo.r0018@gmail.com>
2021-10-08 17:55:39 +0800

    Auto render without loop

--- src/components/Messenger/RetrieveMessage.jsx
+++ src/components/Messenger/RetrieveMessage.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 
 const RetrieveMessage = (props) => {
     const [messages, setMessages] = useState([]);
-    
+
     const recieveData = () => {
         axios.get(`http://206.189.91.54//api/v1/messages?receiver_class=${props.class}&receiver_id=`+props.receiver_id,{
             headers: getToken()
@@ -15,7 +15,7 @@ const RetrieveMessage = (props) => {
 
     useEffect(() => {
         recieveData()
-    },[props])
+    },[messages, setMessages])
 
     return ( 
         <div>
