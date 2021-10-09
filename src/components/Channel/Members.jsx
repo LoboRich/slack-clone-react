55b2ec4e392da64a49b400110b707d758c38b9c2
Francis Paolo <francispaolo.talan@gmail.com>
2021-10-09 13:06:28 +0800

    Additional Changes for DM Functionality and Modal

--- src/components/Channel/Members.jsx
+++ src/components/Channel/Members.jsx
@@ -1,15 +1,59 @@
 import AddMembers from './AddMembers';
 import './Channel.css'
+import React from 'react'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { getToken } from '../../Utils/common';
+import { useHistory } from "react-router-dom";
+import './Channel.css'
+import Search from '../Search';
 
 
 const Members = (props) => {
+
+    const history = useHistory();
+    const [details, setDetails] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+    const [hasError, setErrors] = useState(false);
+
+    useEffect(() => {
+        axios.get("http://206.189.91.54//api/v1/channels/"+props.channel_id, {
+            headers: getToken()
+        }).then((res) => {
+            setDetails(res['data']['data']);
+            setLoading(false);
+        }).catch(error => {
+            setErrors(error)
+            setLoading(true)
+        })
+    },[props]);
+
+    const newDM = (user_id) => {
+        history.push('/users/User/'+user_id)
+    }
+
+    console.log(props.class)
+
+
     return ( 
         <div className="membersContainer">
             {/* Add Members to Channel */}
             {/* List of Members */}
             <div className="membersForm">
-                <AddMembers channel_id={props.channel_id} />
-            </div>
+             {if(props.receiver_class){
+                 !isLoading ? (
+                    details['channel_members'].map(detail => {
+                        const {user_id} = detail;
+                        return (
+                            <span onClick={() => newDM(user_id)} user_id={user_id} className='memberLink'><Search user_id={user_id}/></span>
+                        );
+                    })
+                ): (
+                <p>{isLoading}</p>
+                )
+             }}   
+                    <AddMembers channel_id={props.channel_id} />
+                </div>
         </div>
      );
 }
