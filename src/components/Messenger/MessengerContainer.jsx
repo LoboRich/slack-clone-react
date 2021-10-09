8f5cb64f95534c57acc40c135fca0f40e708b74f
Richelle Lobo <lobo.r0018@gmail.com>
2021-10-09 14:35:37 +0800

    Display User details && messages

--- src/components/Messenger/MessengerContainer.jsx
+++ src/components/Messenger/MessengerContainer.jsx
@@ -7,16 +7,26 @@ import message from './MessengerContainer.module.css'
 import ChannelDetails from "../Channel/ChannelDetails";
 import SendMessage from './SendMessage'
 import Members from "../Channel/Members";
+import UserDetails from "../Users/UserDetails";
 
 
 const MessageContainer = (props) => {
+    const receiver_class = props.match.params.class
+    const receiver_id = props.match.params.id
 
     return ( 
     <div className={message.messageWrapper}>
-        <Members channel_id={props.match.params.id}/>
-        <ChannelDetails channel_id={props.match.params.id} />
-        <RetrieveMessage receiver_class={props.match.params.class} receiver_id={props.match.params.id} />
-        <SendMessage receiver_class={props.match.params.class} receiver_id={props.match.params.id}/>
+        { receiver_class === 'Channel' ? (
+            <>
+                <Members channel_id={receiver_id}/>
+                <ChannelDetails channel_id={receiver_id} />
+            </>
+        ):(
+            <UserDetails user_id={receiver_id}/>
+        )
+        }
+        <RetrieveMessage receiver_class={receiver_class} receiver_id={receiver_id} />
+        <SendMessage receiver_class={receiver_class} receiver_id={receiver_id}/>
     </div> 
     );
 }
