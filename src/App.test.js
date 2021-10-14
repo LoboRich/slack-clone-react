7d5abba71ff5a279d9ce5d29cf5dc44d3eb314e5
Richelle Lobo <lobo.r0018@gmail.com>
2021-10-14 21:44:47 +0800

    WIP Login component test scripts

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
+import Login from './components/Authenticate/Login';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+
+describe('Login page', () => {
+  test('renders button with text Sign with Email', () => {
+    render(<Login />);
+    const linkElement = screen.getByText(/Sign In with Email/i);
+    expect(linkElement).toBeInTheDocument();    
+  });
+
+  test('render error when submit', () => {
+    render(<Login />);
+    // expect(screen.getByRole('input', { name: 'email' })).toHaveValue('test');
+    const email = screen.getByPlaceholderText('Your E-mail')
+    fireEvent.change(email, { target: { value: 'test69@gmail.com' } })
+
+    const password = screen.getByPlaceholderText('Your Password')
+    fireEvent.change(email, { target: { value: '1234566' } })
+
+    
+    const submit_button = screen.getByText(/Sign In with Email/i);
+    userEvent.click(submit_button);
+
+    const noticeElement = screen.getByText(/Sign in to your account/i);
+    // expect(noticeElement).toBeInTheDocument(); 
+    // expect(input).toHaveValue('123')
+  });
+
+}) 
