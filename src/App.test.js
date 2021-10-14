894ada5631326d359255dc0afd994cc19fe621aa
Richelle Lobo <lobo.r0018@gmail.com>
2021-10-14 22:26:35 +0800

    WIP Login component test scripts

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,19 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 import userEvent from '@testing-library/user-event';
 import Login from './components/Authenticate/Login';
 
 
-
 describe('Login page', () => {
+  
   test('renders button with text Sign with Email', () => {
     render(<Login />);
     const linkElement = screen.getByText(/Sign In with Email/i);
     expect(linkElement).toBeInTheDocument();    
   });
 
-  test('render error when submit', () => {
+  test('Must fail to login with invalid credentials', async () => {
     render(<Login />);
-    // expect(screen.getByRole('input', { name: 'email' })).toHaveValue('test');
     const email = screen.getByPlaceholderText('Your E-mail')
     fireEvent.change(email, { target: { value: 'test69@gmail.com' } })
 
@@ -25,9 +24,23 @@ describe('Login page', () => {
     const submit_button = screen.getByText(/Sign In with Email/i);
     userEvent.click(submit_button);
 
-    const noticeElement = screen.getByText(/Sign in to your account/i);
-    // expect(noticeElement).toBeInTheDocument(); 
-    // expect(input).toHaveValue('123')
+    await waitFor(() => {
+      expect(screen.getByText('Invalid login credentials. Please try again.')).toBeInTheDocument()
+    })
+  });
+
+  test('Must redirect to home page after login with valid credentials', async () => {
+    render(<Login />);
+    const email = screen.getByPlaceholderText('Your E-mail')
+    fireEvent.change(email, { target: { value: 'test69@gmail.com' } })
+
+    const password = screen.getByPlaceholderText('Your Password')
+    fireEvent.change(email, { target: { value: '123456' } })
+
+    const submit_button = screen.getByText(/Sign In with Email/i);
+    userEvent.click(submit_button);
+
+    await screen.findByText('Welcome to Slack!')
   });
 
 }) 
