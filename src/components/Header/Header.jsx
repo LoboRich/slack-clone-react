d03007a5a73e0e06d1454633fe67a01a64f3b15c
Richelle Lobo <lobo.r0018@gmail.com>
2021-10-15 13:49:47 +0800

    Adds search function to header's search

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,40 @@ import { Avatar } from '@material-ui/core';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import SearchIcon from '@material-ui/icons/Search';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import axios from 'axios';
+import { getToken } from '../../Utils/common';
+import {useState, useEffect } from 'react';
+import Select from 'react-select';
+import { useHistory } from "react-router-dom";
 
 
 function Header() {
-    // const history = useHistory()
-    // const [user, setUser] = useState(null)
+    const history = useHistory();
+    const [userList, setUserList] = useState([]);
+
+    const fetchUserList = () => {
+      axios.get("http://206.189.91.54//api/v1/users", {
+          headers: getToken()
+      }).then((res) => {
+          setUserList(res['data']['data']);
+      })
+    }
+  
+    useEffect(() => {
+        fetchUserList();
+    }, []);
+
+    const Options = [
+      userList.map(list => {
+          const {id, email} = list;
+          return { value: id, label: email };
+      })
+  ]
+
+  const select_user = (e) => {
+    document.getElementById('user_select')
+    history.push('/users/User/'+e.value)
+  }
 
     return (
       <div className="header">
@@ -15,8 +44,7 @@ function Header() {
           <AccessTimeIcon />
         </div>
         <div className="header__middle">
-          <SearchIcon />
-          <input placeholder="Search" />
+          <Select id="user_select" name="users" onChange={select_user} options={Options[0]} className="basic-multi-select user-select" classNamePrefix="Search" placeholder="Search User"/>
         </div>
         <div className="header__right">
           <HelpOutlineIcon />
