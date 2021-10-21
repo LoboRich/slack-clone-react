a40854423e9694f02de5fc32bdcab1305385ee47
Richelle Lobo <lobo.r0018@gmail.com>
2021-10-21 19:35:20 +0800

    Enable header search to find both user and channel

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,9 +6,11 @@ import {useState, useEffect, useRef } from 'react';
 import { getDatabase, ref, onValue } from "firebase/database";
 import Select from 'react-select';
 import { useHistory } from "react-router-dom";
-import {FetchUsers} from '../../Utils/Api'
+import {FetchUserDms, FetchUserChannels } from '../../Utils/Api'
 import User from './User';
 import man from '../resources/man.png'
+import lock from '../resources/lock.png'
+import avatar from '../resources/avatar.png'
 
 function useOutsideAlerter(ref, setuserModal) {
   useEffect(() => {
@@ -31,53 +33,94 @@ function useOutsideAlerter(ref, setuserModal) {
 }
 
 function Header() {
-    const db = getDatabase();
-    const history = useHistory();
-    const [userList, setUserList] = useState([]);
-    const [userModal, setuserModal] = useState(false)
-    const wrapperRef = useRef(null);
-    useOutsideAlerter(wrapperRef, setuserModal);
+  const db = getDatabase();
+  const history = useHistory();
+  const [userList, setUserList] = useState([]);
+  const [channels, setChannels] = useState([])
+  const [userModal, setuserModal] = useState(false)
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef, setuserModal);
 
-    const list = () => {
-      FetchUsers(getToken())
-        .then(res => {
-          setUserList(res['data']['data']);
-        }).catch(err => err)
-    }
+  const getDirectMessages = () => {
+    FetchUserDms(getToken())
+      .then(res => {
+        if(res['data']['data'] === undefined){
+            return
+        }else{
+          setUserList(res['data']['data'])
+        }
+      })
+  }
 
-    useEffect(() => {
-      list();
-      const new_user = ref(db, '/new_user');
-        onValue(new_user, (snapshot) => {
-          const new_user = snapshot.val(); 
-          list();
-      });
-    }, []);
+  const getChannels = () => {
+    FetchUserChannels(getToken())
+      .then(res => {
+          if(res['data']['data'] === undefined){
+              return
+          }else{
+              setChannels(res['data']['data'])
+          }
+      })
+  }
 
   const Options = [
     userList.map(list => {
         const {id, email} = list;
-        return { value: id, label: email };
+        return {
+          value: `/users/User/${id}`,
+          label: <div className='search_output_container'>
+            <img src={avatar} className='search_img'/>
+            {email}
+          </div> 
+        };
+    }),
+    
+  ]
+  const Options1 = [
+    channels.map(c => {
+      const {id, name} = c;
+      return {
+        value: `/channel-details/Channel/${id}`,
+        label: <div className='search_output_container'>
+          <img src={lock} className='search_img'/>
+          {name}
+          </div> 
+        };
     })
   ]
 
+  useEffect(() => {
+    getChannels();
+    getDirectMessages();
+    const channel = ref(db, `/channel`);
+    onValue(channel, (snapshot) => {
+        const channel = snapshot.val();
+        getChannels();
+    });
+
+    const dm = ref(db, `/chats/User`);
+    onValue(dm, (snapshot) => {
+        const dm = snapshot.val();
+        getDirectMessages();
+    });
+  }, []);
+
   const select_user = (e) => {
-    history.push('/users/User/'+e.value)
+    history.push(e.value)
   }
 
   return (
     <div className="header">
-      <button className='navBtn'>Hey</button>
       <div className="header__left">
         <AccessTimeIcon />
       </div>
       <div className="header__middle">
-        <Select id="user_select" name="users" onChange={select_user} options={Options[0]} className="basic-multi-select user-select" classNamePrefix="Search" placeholder="Search User"/>
+        <Select id="user_select" name="users" onChange={select_user} options={[...Options[0], ...Options1[0]]} className="basic-multi-select user-select" classNamePrefix="Search" placeholder="Search User/Channel"/>
       </div>
       <div className="header__right">
         <HelpOutlineIcon />
+        <img src={man} alt="" className="header__avatar" onClick={()=>setuserModal(true)}/>
       </div> 
-      <img src={man} alt="" className="header__avatar" onClick={()=>setuserModal(true)}/>
       {userModal && <div className="userInfoModal" ref={wrapperRef}><User modalInt={setuserModal}/></div>}
     </div>
   )
